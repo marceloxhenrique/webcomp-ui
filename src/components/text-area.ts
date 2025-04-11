60dc46517dc2e766338fadf8d7040d8cdde9fcc8
Marcelo da Silva <marceloxhenrique@hotmail.com>
2025-04-11 22:43:43 +0200

    feat: Add TextArea component

---
+++ src/components/text-area.ts
@@ -0,0 +1,85 @@
+export default class Textarea extends HTMLElement {
+  template: HTMLTemplateElement;
+  inputLabel: string;
+  inputPlaceholder: string;
+  inputWidth: string;
+  rows: string;
+  constructor() {
+    super();
+    this.attachShadow({mode: "open"});
+    this.template = document.createElement("template");
+    this.inputLabel = this.getAttribute("input-label") || "Textarea";
+    this.inputPlaceholder = this.getAttribute("input-placeholder") || "Type here";
+    this.inputWidth = this.getAttribute("input-width") || "25rem";
+    this.rows = this.getAttribute("rows") || "4"; // Default rows to 4
+    this.render();
+  }
+
+  static get observedAttributes() {
+    return ["input-label", "input-placeholder", "input-width", "rows"];
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (oldValue !== newValue) {
+      const attributeMap: Record<string, keyof this> = {
+        "input-label": "inputLabel",
+        "input-placeholder": "inputPlaceholder",
+        "input-width": "inputWidth",
+        rows: "rows",
+      };
+      if (attributeMap[name]) {
+        (this[attributeMap[name]] as string) = newValue;
+        this.render();
+      }
+    }
+  }
+
+  render() {
+    this.shadowRoot!.innerHTML = ``;
+    this.template.innerHTML = `
+    <style>
+      *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      #textarea{
+        width: ${this.inputWidth};
+        padding: 0.5rem;
+        border: .12rem solid #cfcfd2;
+        border-radius: 0.3rem;
+        font-family: Arial, Helvetica, sans-serif;
+        height: auto;
+        font-size: 1rem;
+        outline: none;
+        resize: vertical;
+      }
+      #textarea:focus{
+        border: .14rem solid rgb(10, 10, 10);
+      }
+      .label{
+        width: 100%;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 1rem;
+        margin-bottom: 0.2rem;
+        display: block;
+        color: #303131;
+      }
+    </style>
+    <label for="textarea" class="label">${this.inputLabel}</label>
+    <textarea id="textarea" placeholder="${this.inputPlaceholder}" rows="${this.rows}" class="textarea"></textarea>
+    `;
+    this.shadowRoot?.appendChild(this.template.content.cloneNode(true));
+
+    const textareaElement = this.shadowRoot?.querySelector("#textarea");
+    textareaElement?.addEventListener("input", (event) => {
+      this.dispatchEvent(
+        new CustomEvent("inputChange", {
+          detail: {value: (event.target as HTMLTextAreaElement).value},
+          bubbles: true,
+          composed: true,
+        })
+      );
+    });
+  }
+}
--- src/lib.ts
+++ src/lib.ts
@@ -1,17 +1,23 @@
+import Spinner from "./components/spinner.js";
+customElements.define("w-spinner", Spinner);
 import Card from "./components/card.js";
-import Button from "./components/button.js";
-import Input from "./components/input.js";
-import Accordion from "./components/accordion.js";
-
 customElements.define("w-card", Card);
+import Button from "./components/button.js";
 customElements.define("w-button", Button);
+import Input from "./components/input.js";
 customElements.define("w-input", Input);
+import Accordion from "./components/accordion.js";
 customElements.define("w-accordion", Accordion);
-export {Card, Button, Input, Accordion};
+import Textarea from "./components/text-area.js";
+customElements.define("w-textarea", Textarea);
+
+export {Card, Button, Input, Accordion, Spinner};
 
 export default {
   Card,
   Button,
   Input,
   Accordion,
+  Spinner,
+  Textarea,
 };
